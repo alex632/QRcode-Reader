36bbe6dd86ecc47ad40fa22a4c63749a1a8391d1
Alex Huang <alex_huang@wistron.com>
2018-01-16 21:16:39 +0800

    modified

--- www/js/index.js
+++ www/js/index.js
@@ -44,33 +44,3 @@ var app = {
 };
 
 app.initialize();
-
-/*
- * From maduka的技術日記
- */
-function funScan()
-{
-    var scanOption =
-    {
-        "preferFrontCamera": true, // iOS and Android
-        "showFlipCameraButton": true, // iOS and Android
-        "prompt": "Place a barcode inside the scan area", // supported on Android only
-        "formats": "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-        "orientation": "landscape" // Android only (portrait|landscape), default unset so it rotates with the device
-    };
-
-    cordova.plugins.barcodeScanner.scan(funScanSuccess, funScanFail, scanOption);
-}
-
-function funScanSuccess(result)
-{
-    alert("We got a barcode\n" +
-            "Result: " + result.text + "\n" +
-            "Format: " + result.format + "\n" +
-            "Cancelled: " + result.cancelled);
-}
-
-function funScanFail(error)
-{
-    alert("Scanning failed: " + error);
-}
